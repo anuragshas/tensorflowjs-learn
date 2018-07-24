bd4abebda4b6bb5b84711f10bc1d13e39b3fe4ed
anurag <anurag.comm@gmail.com>
2018-07-24 07:00:03 +0530

    added linear reggression

---
+++ src/client/data/LinearRegression.js
@@ -0,0 +1,29 @@
+import * as tf from '@tensorflow/tfjs';
+
+export function generateData(numPoints, coeff, sigma = 0.04) {
+  return tf.tidy(() => {
+    const [a, b] = [
+      tf.scalar(coeff.a), tf.scalar(coeff.b)
+    ];
+
+    const xs = tf.randomUniform([numPoints], -1, 1);
+
+    // Generate data
+    const ys = a.mul(xs)
+      .add(b)
+      // Add random noise to the generated data
+      // to make the problem a bit more interesting
+      .add(tf.randomNormal([numPoints], 0, sigma));
+
+    // Normalize the y values to the range 0 to 1.
+    const ymin = ys.min();
+    const ymax = ys.max();
+    const yrange = ymax.sub(ymin);
+    const ysNormalized = ys.sub(ymin).div(yrange);
+
+    return {
+      xs,
+      ys: ysNormalized
+    };
+  });
+}
---
+++ src/client/train/LinearRegression.js
@@ -0,0 +1,105 @@
+import * as tf from '@tensorflow/tfjs';
+import { generateData } from '../data/LinearRegression';
+
+/**
+ * We want to learn the coefficients that give correct solutions to the
+ * following cubic equation:
+ *      y = a * x + b
+ * In other words we want to learn values for:
+ *      a
+ *      b
+ * Such that this function produces 'desired outputs' for y when provided
+ * with x. We will provide some examples of 'xs' and 'ys' to allow this model
+ * to learn what we mean by desired outputs and then use it to produce new
+ * values of y that fit the curve implied by our example.
+ */
+
+// Step 1. Set up variables, these are the things we want the model
+// to learn in order to do prediction accurately. We will initialize
+// them with random values.
+const a = tf.variable(tf.scalar(Math.random()));
+const b = tf.variable(tf.scalar(Math.random()));
+
+// Step 2. Create an optimizer, we will use this later. You can play
+// with some of these values to see how the model performs.
+const numIterations = 75;
+const learningRate = 0.5;
+const optimizer = tf.train.sgd(learningRate);
+
+// Step 3. Write our training process functions.
+
+/*
+ * This function represents our 'model'. Given an input 'x' it will try and
+ * predict the appropriate output 'y'.
+ *
+ * It is also sometimes referred to as the 'forward' step of our training
+ * process. Though we will use the same function for predictions later.
+ *
+ * @return number predicted y value
+ */
+function predict(x) {
+  // y = a * x  + b
+  return tf.tidy(() => {
+    return a.mul(x)
+      .add(b);
+  });
+}
+
+/*
+ * This will tell us how good the 'prediction' is given what we actually
+ * expected.
+ *
+ * prediction is a tensor with our predicted y values.
+ * labels is a tensor with the y values the model should have predicted.
+ */
+function loss(prediction, labels) {
+  // Having a good error function is key for training a machine learning model
+  const error = prediction.sub(labels).square().mean();
+  return error;
+}
+
+/*
+ * This will iteratively train our model.
+ *
+ * xs - training data x values
+ * ys — training data y values
+ */
+async function train(xs, ys, numIterations) {
+  for (let iter = 0; iter < numIterations; iter++) {
+    // optimizer.minimize is where the training happens.
+
+    // The function it takes must return a numerical estimate (i.e. loss)
+    // of how well we are doing using the current state of
+    // the variables we created at the start.
+
+    // This optimizer does the 'backward' step of our training process
+    // updating variables defined previously in order to minimize the
+    // loss.
+    optimizer.minimize(() => {
+      // Feed the examples into the model
+      const pred = predict(xs);
+      return loss(pred, ys);
+    });
+
+    // Use tf.nextFrame to not block the browser.
+    await tf.nextFrame();
+  }
+}
+
+async function learnCoefficients() {
+  const trueCoefficients = { a: -.8, b: -.2 };
+  const trainingData = generateData(100, trueCoefficients);
+
+  const predictionsBefore = predict(trainingData.xs);
+
+  // Train the model!
+  await train(trainingData.xs, trainingData.ys, numIterations);
+
+  const predictionsAfter = predict(trainingData.xs);
+
+  predictionsBefore.dispose();
+  predictionsAfter.dispose();
+}
+
+
+learnCoefficients();
